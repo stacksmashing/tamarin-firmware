a0664564967c47c2e970b4ff8ef368709adabe1a
Thomas Roth <code@stacksmashing.net>
2023-04-30 19:40:07 +0200

    Add command for re-enumeration, command for firmware update, and USB charging cable identification. Also improve lightning performance.

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 #include "pico/stdio.h"
 #include "pico/multicore.h"
+#include "pico/bootrom.h"
 
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
@@ -48,7 +49,7 @@ void lightning_send_wake() {
     
     gpio_set_dir(PIN_SDQ, GPIO_IN);
     
-    sleep_us(1000);
+    sleep_us(50);
 }
 
 void tamarin_reset_tristar(PIO pio, uint sm) {
@@ -72,6 +73,10 @@ enum state {
     WAITING_FOR_INIT,
     READING_TRISTAR_REQUEST,
     HANDLE_TRISTAR_REQUEST,
+    READING_POWER_REQUEST,
+    HANDLE_POWER_REQUEST,
+    READING_TRISTAR_UNKNOWN_76,
+    HANDLE_TRISTAR_UNKNOWN_76,
     HANDLE_JTAG,
     // Force JTAG mode if the cable is already in JTAG mode
     // (e.g. after the tamarin cable was reset but not the device)
@@ -91,7 +96,8 @@ enum command {
 
 enum default_command {
     DEFAULT_CMD_DCSD = 0,
-    DEFAULT_CMD_JTAG
+    DEFAULT_CMD_JTAG,
+    DEFAULT_CMD_CHARGING
 };
 
 // Command that should be sent automatically (on reboot, plugin, etc.)
@@ -110,7 +116,14 @@ enum responses {
     RSP_RESET,
     // DFU
     RSP_DFU,
+    RSP_USB_A_CHARGING_CABLE,
     RSP_MAX
+};
+
+enum TRISTAR_REQUESTS {
+    TRISTAR_POLL = 0x74,
+    TRISTAR_POWER = 0x70,
+    TRISTAR_UNKNOWN_76 = 0x76,
 };
 
 // To generate CRCs:
@@ -120,6 +133,7 @@ const uint8_t bootloader_response[RSP_MAX][8] = {
     [RSP_USB_UART] = {0x75, 0x20, 0x00, 0x10, 0x00, 0x00, 0x00, 0x92},
     [RSP_RESET] = {0x75, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x83},
     [RSP_DFU] = {0x75, 0x20, 0x00, 0x02, 0x00, 0x00, 0x00, 0xad},
+    [RSP_USB_A_CHARGING_CABLE] = {0x75, 0x10, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x66},
 };
 
 void set_idbus_high_impedance() {
@@ -159,12 +173,14 @@ void respond_lightning(PIO pio, uint sm, const uint8_t *data, size_t data_length
     pio_clear_instruction_memory(pio);
     uint offset = pio_add_program(pio, &lightning_tx_program);
     pio_sm_config c = lightning_tx_program_init(pio, sm, offset, PIN_SDQ, 125.0/2.0);
-    for(size_t i=0; i < data_length; i++) {
+    // We fill in 1 byte first, then the second byte, only then do we start to do
+    // "get_blocking" to avoid the state machine ever running empty.
+    pio_sm_put_blocking(pio, sm, data[0]);
+    for(size_t i=1; i < data_length; i++) {
         pio_sm_put_blocking(pio, sm, data[i]);
+        pio_sm_get_blocking(pio, sm);
     }
-    while (!pio_sm_is_tx_fifo_empty(pio, sm)) {
-        sleep_us(500);
-    }
+    pio_sm_get_blocking(pio, sm);
 }
 
 void output_state_machine() {
@@ -189,17 +205,23 @@ void output_state_machine() {
                 value = pio_sm_get_blocking(pio, sm);
                 value_b = reverse_byte(value & 0xFF);
 
-                if(value_b == 0x74) {
+                if(value_b == TRISTAR_POLL) {
                     leave_dcsd();
                     gState = READING_TRISTAR_REQUEST;
+                    buf[0] = value_b;
+                    i = 1;
+                } else if(value_b == TRISTAR_POWER) {
+                    gState = READING_POWER_REQUEST;
+                    buf[0] = value_b;
+                    i = 1;
+                } else if(value_b == TRISTAR_UNKNOWN_76) {
+                    gState = READING_TRISTAR_UNKNOWN_76;
                     buf[0] = value_b;
                     i = 1;
                 } else {
                     serprint("Tristar >> 0x%x (unknown, ignoring)\r\n", value_b);
                 }
-                
                 sleep_us(100); // Breaks without this...
-                
                 break;
             case READING_TRISTAR_REQUEST:
                 if (pio_sm_is_rx_fifo_empty(pio, sm)) break;
@@ -211,11 +233,46 @@ void output_state_machine() {
                     gState = HANDLE_TRISTAR_REQUEST;
                     i = 0;
                 }
+                sleep_us(10); // Breaks without this...
+                break;
+            case READING_TRISTAR_UNKNOWN_76:
+                if (pio_sm_is_rx_fifo_empty(pio, sm)) break;
+                value = pio_sm_get_blocking(pio, sm);
+                value_b = reverse_byte(value & 0xFF);
+
+                buf[i++] = value_b;
+                if(i == 2) {
+                    gState = HANDLE_TRISTAR_UNKNOWN_76;
+                    i = 0;
+                }
+                sleep_us(10); // Breaks without this...
+                break;
+            case HANDLE_TRISTAR_UNKNOWN_76:
+                respond_lightning(pio, sm, "\x77\x02\x01\x02\x80\x60\x01\x39\x3a\x44\x3e\xc9", 12);
+                serprint("76 request received: %02X %02X\r\n", buf[0], buf[1]);
+                gState = WAITING_FOR_INIT;
+                break;
+            case READING_POWER_REQUEST:
+                if (pio_sm_is_rx_fifo_empty(pio, sm)) break;
+                value = pio_sm_get_blocking(pio, sm);
+                value_b = reverse_byte(value & 0xFF);
+
+                buf[i++] = value_b;
+                if(i == 4) {
+                    gState = HANDLE_POWER_REQUEST;
+                    i = 0;
+                }
                 
-                sleep_us(100); // Breaks without this...
+                sleep_us(10); // Breaks without this...
                 
                 break;
+            case HANDLE_POWER_REQUEST:
+                respond_lightning(pio, sm, "\x71\x93", 2);
+                serprint("Power request received: %02X %02X %02X %02X\r\n", buf[0], buf[1], buf[2], buf[3]);
+                gState = WAITING_FOR_INIT;
+                break;
             case HANDLE_TRISTAR_REQUEST:
+                serprint("Tristar request received: %02X %02X %02X %02X\r\n", buf[0], buf[1], buf[2], buf[3]);
                 switch(gCommand) {
                     case CMD_DEFAULT:
                         switch (gDefaultCommand) {
@@ -228,6 +285,11 @@ void output_state_machine() {
                                 respond_lightning(pio, sm, bootloader_response[RSP_USB_UART_JTAG], 8);
                                 gState = FORCE_JTAG;
                                 continue;
+                            case DEFAULT_CMD_CHARGING:
+                                respond_lightning(pio, sm, bootloader_response[RSP_USB_A_CHARGING_CABLE], 8);
+                                serprint("Sent charging\r\n");
+                                gState = WAITING_FOR_INIT;
+                                break;
                         }
                         break;
                     case CMD_RESET:
@@ -244,6 +306,7 @@ void output_state_machine() {
                     case CMD_INTERNAL_AUTO_DFU_2:
                         respond_lightning(pio, sm, bootloader_response[RSP_DFU], 8);
                         serprint("Device should now be in DFU mode.\r\n");
+                        gCommand = CMD_DEFAULT;
                         break;
                     default:
                         serprint("UNKNOWN MODE. Send help. Locking up.\r\n");
@@ -274,14 +337,17 @@ void print_menu() {
     serprint("2: DCSD mode\r\n");
     serprint("3: Reset device\r\n");
     serprint("4: Reset and enter DFU mode\r\n");
+    serprint("5: Reenumerate\r\n");
     serprint("F: Force JTAG mode without sending command\r\n");
     serprint("R: Reset Tamarin cable\r\n");
+    serprint("U: Go into firmware update mode\r\n");
     serprint("> ");
 }
 
 void shell_task() {
     if (tud_cdc_n_available(ITF_CONSOLE)) {
         char c = tud_cdc_n_read_char(ITF_CONSOLE);
+        tud_cdc_n_write_char(ITF_CONSOLE, c);
         switch(c) {
             case '1':
                 serprint("\r\nEnabling JTAG mode.\r\n");
@@ -305,6 +371,11 @@ void shell_task() {
                 gCommand = CMD_AUTO_DFU;
                 gState = RESTART_ENUMERATION;
                 break;
+            case '5':
+                serprint("\r\nReenumerate\r\n");
+                gCommand = CMD_DEFAULT;
+                gState = RESTART_ENUMERATION;
+                break;
             case 'f':
             case 'F':
                 serprint("\r\nForcing JTAG mode.\r\n");
@@ -315,6 +386,9 @@ void shell_task() {
             case 'r':
                 watchdog_enable(100, 1);
                 break;
+            case 'U':
+            case 'u':
+                reset_usb_boot(0, 0);
             default:
                 print_menu();
                 break;
@@ -323,6 +397,12 @@ void shell_task() {
 }
 
 int main() {
+    // Power enable for Tamarin Cable. Not required on regular Pico,
+    // but also doesn't hurt.
+    gpio_init(19);
+    gpio_set_dir(19, GPIO_OUT);
+    gpio_put(19, 1);
+
     board_init();
     tusb_init();
 
